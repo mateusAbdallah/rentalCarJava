63e39b5ef25276e4140b0ff75a4cf35c37149896
Mateus Abdallah <abdallahmateus@gmail.com>
2023-05-06 16:56:49 -0700

    set action listener in the button

--- Main.java
+++ Main.java
@@ -20,6 +20,7 @@ import java.nio.file.Files;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -29,6 +30,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class Main extends JFrame implements ActionListener {
 	
@@ -102,7 +104,25 @@ public class Main extends JFrame implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		Vehicle vehicle = (Vehicle) vehicleComboBox.getSelectedItem();
+		int day = (Integer) dayComboBox.getSelectedItem();
+		int month = monthComboBox.getSelectedIndex() + 1;
+		int year = (Integer) yearComboBox.getSelectedItem();
+		int duration = (Integer) durationComboBox.getSelectedItem();
+		LocalDate pickupDate;
+		try {
+			pickupDate = LocalDate.of(year, month, day);
+			
+		}catch(DateTimeException exception) {
+			JOptionPane.showConfirmDialog(this, "Please enter a valid!");
+			return;
+			
+		}
+		LocalDate dropOffDate = pickupDate.plusDays(duration);
+		
+		Rental rental = new Rental (vehicle, pickupDate, dropOffDate);
+		String output = "Successfully created the following rental:\n" + rental;
+		JOptionPane.showConfirmDialog(this, output);
 		
 	}
 	
@@ -114,65 +134,6 @@ public class Main extends JFrame implements ActionListener {
 	      rentalProgram.setVisible(true);		
 	}
 
-	//private static final OpenOption CREATE = null;
-
-	/*public static void main(String[] args) {
-		
-		int selection = 0;
-		Vehicle vehicles[] = null;
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please choose from the following options:");
-		System.out.println("1 - Enter Vehicles:");
-		System.out.println("2 - Load Vehicles:");
-		selection = input.nextInt();
-		
-		if(selection == 1) {
-			vehicles = createVehicles();
-			storeVehicles(vehicles, "NewVehicles.txt");
-			
-		}
-		else {
-			System.out.println("Please enter file name");
-			input.nextLine();
-			String fileName = input.nextLine();
-			vehicles = loadVehicles(fileName);
-		}
-		
-		System.out.println("Available vehicles:");
-		for(int i = 0; i < vehicles.length; i++) {
-			System.out.println((i+1 + " " + vehicles[i].getDescription()));
-		}
-		
-		boolean success = false;
-		
-		while(!success) {
-			try {
-				System.out.println("For pick enter vehicle's number");
-				selection = input.nextInt();
-				if(selection >= 1 && selection <= vehicles.length) {
-					success = true;
-				} else {
-					System.out.println("Enter a valid number: 1 or "+ vehicles.length);
-				}
-			} catch( Exception e) {
-				System.out.println("Enter a valid number: 1 or \"+ vehicles.length");
-				System.out.println(e);
-			}
-		}
-		
-		System.out.println("Enter pick up date:");
-		LocalDate pickUpdate = inputDate();
-		
-		System.out.println("Enter a drop off date");
-		LocalDate dropOffDate = inputDate();
-		
-		Rental rental = new Rental(vehicles[selection - 1], pickUpdate, dropOffDate);
-		
-		System.out.println(rental.getDescription());
-
-	}*/
-	
 	
 	public static Vehicle[] createVehicles() {
 		
